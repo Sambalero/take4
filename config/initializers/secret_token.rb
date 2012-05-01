255ac714983beb3fa75833524b81c07a52ceacf4
Kevin Swallow <kevinswallow2@hotmail.com>
2012-04-30 17:05:15 -0700

    commit 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Take4::Application.config.secret_token = '257ab3f89d72d8a241b27afaf10aea3e96bbbef41cd33c5c9ca1b408e7f9a87aac7b02bff7358b123cf616f6cc9d5dbcebfe3105297a227c8396e1453afbb799'
